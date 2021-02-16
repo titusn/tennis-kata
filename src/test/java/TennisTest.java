f4ecb85575030380d98e185d2a916bb897de8cf9
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2021-02-16 11:20:13 +0100

    Refactor to use only one instance of Player in test

--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -5,15 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TennisTest {
+    Player player = new Player();
+
     @Test
     public void newPlayerShouldHaveScoreZero() {
-        Player player = new Player();
         assertEquals(0, player.getScore());
     }
 
     @Test void increaseScoreOnceShouldSetScoreTo15() {
-        Player player = new Player();
         player.increaseScore();
         assertEquals(15, player.getScore());
     }
+
 }
