467f4c2791675ae7f6ccbfb6a548b2a047e65dea
Titus Nachbauer <titus.nachbauer@nl.abnamro.com>
2021-02-16 11:12:54 +0100

    Add Player class

---
+++ src/main/java/com/titusnachbauer/tennis/Player.java
@@ -0,0 +1,7 @@
+package com.titusnachbauer.tennis;
+
+public class Player {
+    public int getScore() {
+        return 0;
+    }
+}
--- src/test/java/TennisTest.java
+++ src/test/java/TennisTest.java
@@ -1,8 +1,13 @@
+import com.titusnachbauer.tennis.Player;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class TennisTest {
     @Test
-    public void nothing() {
-
+    public void newPlayerShouldHaveScoreZero () {
+        Player player = new Player();
+        assertEquals(0, player.getScore());
     }
 }
